d5b79a6bf8c646f9cc9645923ccc21e24bd4bf0c
Sudhakara Sirivar <SUDHAKARAS@BIRLASOFT.COM>
2025-02-12 13:20:17 +0530

    angular js file added.

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-var student = {
-  name: "",
-  type: "student"
-};
+(function(){
 
-document.addEventListener('DOMContentLoaded', contentLoaded);
+'use strict';
 
-function contentLoaded(event) {
-  document.getElementById('name').addEventListener("keyup", keyUp);
-}
+angular.module('Assignment1App',[])
+.controller('Assignment1Controller', function($scope){
+$scope.lunchNames = "";
 
-function keyUp(event) {
-  calculateNumericOutput();
-}
+});
 
-function calculateNumericOutput() {
-  student.name = document.getElementById('name').value;
-
-  var totalNameValue = 0;
-  for (var i = 0; i < student.name.length; i++) {
-    totalNameValue += student.name.charCodeAt(i);
-  }
-
-  // Insert result into page
-  var output = "Total Numeric value of person's name is " + totalNameValue;
-  document.getElementById('output').innerText = output;
-}
+})();
